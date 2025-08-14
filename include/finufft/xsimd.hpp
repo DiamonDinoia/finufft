aee2cfd1632fae9536193ea27038642bdd9f1f0d
Marco Barbone <mbarbone@flatironinstitute.org>
2025-08-14 06:41:16 -0400

    re-trying xsimd

--- include/finufft/xsimd.hpp
+++ include/finufft/xsimd.hpp
@@ -1,15 +1,14 @@
 #ifndef FINUFFT_XSIMD_HPP
 #define FINUFFT_XSIMD_HPP
 
-#include <xsimd/xsimd.hpp>
+#include <xsimd/config/xsimd_config.hpp>
 
 #ifdef XSIMD_NO_SUPPORTED_ARCHITECTURE
 #undef XSIMD_NO_SUPPORTED_ARCHITECTURE
-#undef XSIMD_CONFIG_HPP
-#undef XSIMD_HPP
 #define XSIMD_WITH_EMULATED 1
 #define XSIMD_DEFAULT_ARCH  emulated<128>
-#include <xsimd/xsimd.hpp>
 #endif
+
+#include <xsimd/xsimd.hpp>
 
 #endif // FINUFFT_XSIMD_HPP
